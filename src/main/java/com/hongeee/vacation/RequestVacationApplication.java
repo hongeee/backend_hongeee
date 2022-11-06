b6a99c26a165aaa0af1ee5bd8bdd3e7ac0520318
hongeee <87ishong@naver.com>
2022-11-06 17:04:42 +0900

    Initial Commit

---
+++ src/main/java/com/hongeee/vacation/RequestVacationApplication.java
@@ -0,0 +1,13 @@
+package com.hongeee.vacation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestVacationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestVacationApplication.class, args);
+	}
+
+}
